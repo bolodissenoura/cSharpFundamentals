ec1c91e0bd08454100cdf79592571df6fda5400c
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-04-24 22:14:27 -0300

    ༼ つ ◕_◕ ༽つ Learning C#

---
+++ Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("First value: ");
+string? input1 = Console.ReadLine();
+Console.WriteLine("Second value: ");
+string? input2 = Console.ReadLine();
+
+float v1 = float.Parse(input1 ?? "0");
+float v2 = float.Parse(input2 ?? "0");
+
+float result = v1 + v2;
+
+Console.WriteLine("Your Value is: " + result);
